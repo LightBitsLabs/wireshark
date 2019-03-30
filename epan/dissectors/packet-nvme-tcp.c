5fd38f1a2229fc29c425ede574ec93d4f7305b63
Solganik Alexander <solganik@gmail.com>
2019-03-30 22:57:27 +0300

    nvme/tcp: dissect nvme-tcp packet header
    This patch performs basic dissection of 8-bytes nvme-tcp header.
    
    Change-Id: I77f7d309b81094e6c38d0bd33f09f1c91cccb46f

--- epan/dissectors/packet-nvme-tcp.c
+++ epan/dissectors/packet-nvme-tcp.c
@@ -52,6 +52,44 @@ static dissector_handle_t nvmet_tcp_handle;
 #define PDU_LEN_OFFSET_FROM_HEADER 4
 static range_t *gPORT_RANGE;
 
+enum nvme_tcp_pdu_type {
+    nvme_tcp_icreq = 0x0,
+    nvme_tcp_icresp = 0x1,
+    nvme_tcp_h2c_term = 0x2,
+    nvme_tcp_c2h_term = 0x3,
+    nvme_tcp_cmd = 0x4,
+    nvme_tcp_rsp = 0x5,
+    nvme_tcp_h2c_data = 0x6,
+    nvme_tcp_c2h_data = 0x7,
+    nvme_tcp_r2t = 0x9,
+};
+
+static const value_string nvme_tcp_pdu_type_vals[] = {
+    { nvme_tcp_icreq, "ICREQ" },
+    { nvme_tcp_icresp, "ICRESP" },
+    { nvme_tcp_h2c_term, "H2CTerm " },
+    { nvme_tcp_c2h_term, "C2HTerm" },
+    { nvme_tcp_cmd, "Command" },
+    { nvme_tcp_rsp, "Response" },
+    { nvme_tcp_h2c_data, "H2CData" },
+    { nvme_tcp_c2h_data, "C2HData" },
+    { nvme_tcp_r2t,"Ready To Transmit" },
+    { 0, NULL }
+};
+
+struct nvme_tcp_q_ctx {
+    struct nvme_q_ctx n_q_ctx;
+};
+
+static int hf_nvme_tcp_type = -1;
+static int hf_nvme_tcp_flags = -1;
+static int hf_nvme_tcp_hlen = -1;
+static int hf_nvme_tcp_pdo = -1;
+static int hf_nvme_tcp_plen = -1;
+
+static int hf_nvme_fabrics_cmd_qid = -1;
+
+static gint ett_nvme_tcp = -1;
 
 static guint
 get_nvme_tcp_pdu_len(packet_info *pinfo _U_,
@@ -64,10 +102,53 @@ get_nvme_tcp_pdu_len(packet_info *pinfo _U_,
 
 static int
 dissect_nvme_tcp_pdu(tvbuff_t *tvb,
-                     packet_info *pinfo _U_,
-                     proto_tree *tree _U_,
+                     packet_info *pinfo,
+                     proto_tree *tree,
                      void* data _U_)
 {
+    conversation_t *conversation;
+    struct nvme_tcp_q_ctx *q_ctx;
+    proto_item *ti;
+    int offset = 0;
+    proto_tree *nvme_tcp_tree;
+
+    conversation = find_or_create_conversation(pinfo);
+    q_ctx = (struct nvme_tcp_q_ctx *)
+            conversation_get_proto_data(conversation, proto_nvme_tcp);
+
+    if (!q_ctx) {
+        q_ctx = (struct nvme_tcp_q_ctx *) wmem_alloc0(wmem_file_scope(),
+                sizeof(struct nvme_tcp_q_ctx));
+        q_ctx->n_q_ctx.pending_cmds = wmem_tree_new(wmem_file_scope());
+        q_ctx->n_q_ctx.done_cmds = wmem_tree_new(wmem_file_scope());
+        q_ctx->n_q_ctx.data_requests = wmem_tree_new(wmem_file_scope());
+        q_ctx->n_q_ctx.data_responses = wmem_tree_new(wmem_file_scope());
+        /* Initially set to non-0 so that by default queues are io queues
+         * this is required to be able to dissect correctly even
+         * if we miss connect command*/
+        q_ctx->n_q_ctx.qid = G_MAXUINT16;
+        conversation_add_proto_data(conversation, proto_nvme_tcp, q_ctx);
+    }
+
+    ti = proto_tree_add_item(tree, proto_nvme_tcp, tvb, 0, -1, ENC_NA);
+    nvme_tcp_tree = proto_item_add_subtree(ti, ett_nvme_tcp);
+
+    if (q_ctx->n_q_ctx.qid != G_MAXUINT16)
+        nvme_publish_qid(nvme_tcp_tree, hf_nvme_fabrics_cmd_qid,
+                q_ctx->n_q_ctx.qid);
+
+    proto_tree_add_item(nvme_tcp_tree, hf_nvme_tcp_type, tvb, offset, 1,
+            ENC_NA);
+    proto_tree_add_item(nvme_tcp_tree, hf_nvme_tcp_flags, tvb, offset + 1, 1,
+            ENC_NA);
+    proto_tree_add_item(nvme_tcp_tree, hf_nvme_tcp_hlen, tvb, offset + 2, 1,
+            ENC_NA);
+    proto_tree_add_item(nvme_tcp_tree, hf_nvme_tcp_pdo, tvb, offset + 3, 1,
+            ENC_NA);
+    proto_tree_add_item(nvme_tcp_tree, hf_nvme_tcp_plen, tvb, offset + 4, 4,
+            ENC_LITTLE_ENDIAN);
+    col_set_str(pinfo->cinfo, COL_PROTOCOL, NVME_FABRICS_TCP);
+
     return tvb_reported_length(tvb);
 }
 
@@ -77,6 +158,7 @@ dissect_nvme_tcp(tvbuff_t *tvb,
                  proto_tree *tree,
                  void *data)
 {
+    col_clear(pinfo->cinfo, COL_INFO);
     col_set_str(pinfo->cinfo, COL_PROTOCOL, NVME_FABRICS_TCP);
     tcp_dissect_pdus(tvb, pinfo, tree, TRUE, NVME_TCP_HEADER_SIZE,
             get_nvme_tcp_pdu_len, dissect_nvme_tcp_pdu, data);
@@ -86,8 +168,38 @@ dissect_nvme_tcp(tvbuff_t *tvb,
 
 void proto_register_nvme_tcp(void) {
 
+    static hf_register_info hf[] = {
+       { &hf_nvme_tcp_type,
+           { "Pdu Type", "nvme-tcp.type",
+             FT_UINT8, BASE_DEC, VALS(nvme_tcp_pdu_type_vals),
+             0x0, NULL, HFILL } },
+       { &hf_nvme_tcp_flags,
+           { "Pdu Specific Flags", "nvme-tcp.flags",
+             FT_UINT8, BASE_DEC, NULL, 0x0, NULL, HFILL } },
+       { &hf_nvme_tcp_hlen,
+           { "Pdu Header Length", "nvme-tcp.hlen",
+             FT_UINT8, BASE_DEC, NULL, 0x0, NULL, HFILL } },
+       { &hf_nvme_tcp_pdo,
+           { "Pdu Data Offset", "nvme-tcp.pdo",
+             FT_UINT8, BASE_DEC, NULL, 0x0, NULL, HFILL } },
+       { &hf_nvme_tcp_plen,
+           { "Packet Length", "nvme-tcp.plen",
+            FT_UINT32, BASE_DEC, NULL, 0x0, NULL, HFILL } },
+       { &hf_nvme_fabrics_cmd_qid,
+           { "Cmd Qid", "nvme-tcp.cmd.qid",
+             FT_UINT16, BASE_HEX, NULL, 0x0,
+             "Qid on which command is issued", HFILL } },
+    };
+
+    static gint *ett[] = {
+        &ett_nvme_tcp
+    };
+
     proto_nvme_tcp = proto_register_protocol("NVM Express Fabrics TCP",
             NVME_FABRICS_TCP, "nvme-tcp");
+
+    proto_register_field_array(proto_nvme_tcp, hf, array_length(hf));
+    proto_register_subtree_array(ett, array_length(ett));
 
     nvmet_tcp_handle = register_dissector("nvme-tcp", dissect_nvme_tcp,
             proto_nvme_tcp);
